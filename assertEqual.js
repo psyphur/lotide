fbe57846d18113495affe534f2bf816581fd2eb1
Shaun Yap <me@shaunyap.ca>
2020-10-26 20:11:19 +0000

    Initial commit with assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('✅✅✅ Assertion Passed: ' + actual + ' === ' + expected);
+  } else {
+    console.log('🛑🛑🛑 Assertion Failed: ' + actual + ' !== ' + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Bob the builder", "Bob is a builder");
+assertEqual("Bob the builder", "Bob the builder");
+assertEqual(1, 0);
+assertEqual("1", 1);
