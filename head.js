63ad23f6973ae48e1efb7d64ec5d9b273e1110ff
Shaun Yap <me@shaunyap.ca>
2020-10-26 20:22:27 +0000

    added head.js

---
+++ head.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(inputArray) {
+  if (inputArray.length > 0) {
+    return inputArray[0];
+  } else {
+    return undefined;
+  }
+};
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), 5);
+assertEqual(head([5]), 5);
+
