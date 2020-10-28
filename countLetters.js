b2823db206777ff4bc8fed95bbc7f53fb70054c0
Shaun Yap <me@shaunyap.ca>
2020-10-28 20:11:35 +0000

    Added countLetters.js

---
+++ countLetters.js
@@ -0,0 +1,30 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = (sentence) => {
+  const letterCounter = {};
+
+  for (const letter in sentence) {
+    if (sentence[letter] !== ' ') {
+      letterCounter[sentence[letter]] = 0;
+    }
+  }
+
+  for (count in letterCounter) {
+    for (letter in sentence) {
+      if (sentence[letter] === count) {
+        letterCounter[count]++;
+      }
+    }
+  }
+  return letterCounter;
+}
+
+// console.log(countLetters("hello"));
+// console.log(countLetters("LHL"));
+// console.log(countLetters("lighthouse in the house"));
